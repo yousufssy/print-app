3ed38b5cdb937a098da8b66b05af1bfa5f2eebd5
yousufssy <yousufsaty@gmail.com>
2026-04-09 04:08:10 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
-        proxy: {
-          '/api': {
-            target: 'http://127.0.0.1:8000',
-            changeOrigin: true
-          }
-     }
+    proxy: {
+      '/api': {
+        target: 'https://print-api-production-5ee8.up.railway.app',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
   },
 })
